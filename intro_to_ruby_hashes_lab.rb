8b269c924844cd26f13b68b33ec25fc0ef06ee51
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-12 19:34:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,12 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :names => {},
+	    :names => {
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      :b_and_o_railroad => {},
+	      :shortline => {}
+	    },
 	    :rent_in_dollars => {}
 	  }
 	}
@@ -30,7 +35,12 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {},
-	    :rent_in_dollars => {}
+	    :rent_in_dollars => {
+	      :one_piece_owned =>25,
+	      :two_pieces_owned =>50,
+	      :three_pieces_owned =>100,
+	      :four_pieces_owned =>200
+	    }
 	  }
 	}
 		
